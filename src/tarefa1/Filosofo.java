95319c5d2645df97b1828c13bdc291b636250fb9
João Paulo Andrade Raimundo <62303009+jpegame@users.noreply.github.com>
2025-12-15 15:55:05 -0300

    better log

--- src/tarefa1/Filosofo.java
+++ src/tarefa1/Filosofo.java
@@ -14,29 +14,30 @@ public class Filosofo implements Runnable {
         this.garfoDireito = direito;
     }
 
+    private void log(String text) {
+        System.out.println("Filósofo " + this.nome + " " + text);
+    }
+
     // Funcao que coloca a Thread para dormir entre 1 a 3 segundos
     private void pensar() throws InterruptedException {
-        System.out.println("Filósofo " + this.nome + " está pensando.");
+        this.log("está pensando.");
         Thread.sleep((random.nextInt(3) + 1) * 1000);
     }
 
     private void comer() throws InterruptedException {
-        System.out.println("Filósofo " + this.nome + " está tentando pegar o garfo esquerdo (" 
-                + garfoEsquerdo.getId() + ")");
+        this.log("está tentando pegar o garfo esquerdo (" + garfoEsquerdo.getId() + ")");
 
         // Filosofo tenta primeiramente pegar o garfo esquerdo e ao pegar tenta o direito, 
         // a função synchronized serve para garantir que apenas um filosofo consiga pegar o garfoEsquerdo
         synchronized (garfoEsquerdo) {
-            System.out.println("Filósofo " + this.nome + " pegou o garfo esquerdo (" 
-                    + garfoEsquerdo.getId() + ")");
-            System.out.println("Filósofo " + this.nome + " tentando pegar o garfo direito (" 
-                    + garfoDireito.getId() + ")");
+            this.log("pegou o garfo esquerdo (" + garfoEsquerdo.getId() + ")");
+            this.log("tentando pegar o garfo direito (" + garfoDireito.getId() + ")");
 
             // Mesmo processo acima, mas agora para o direito
             synchronized (garfoDireito) {
-                System.out.println(" \nFilósofo " + this.nome + " pegou ambos os garfos e começou a comer.");
+                this.log("pegou ambos os garfos e começou a comer.");
                 Thread.sleep((random.nextInt(3) + 1) * 1000);
-                System.out.println("Filósofo " + this.nome + " terminou de comer e soltou os garfos.\n");
+                this.log("terminou de comer e soltou os garfos.\\n");
             }
         }
     }
